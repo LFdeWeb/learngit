7cb2d164546ae6a3a4553fc85f12edaac08a4491
LFdeWeb <978955033@qq.com>
2018-04-11 19:20:03 +0800

    infiniti

--- frame/js/common.js
+++ frame/js/common.js
@@ -12,8 +12,21 @@ $('input').on({
 			this.value=this.defaultValue
 		}
 	}
-
+	
 })
+/*弹出视频的图片*/
+$('.img-vi1').click(function() {
+				$('.vid-box1').fadeIn(200);
+				$('.mask').fadeIn(200);
+			})
+$('.img-vi2').click(function() {
+				$('.vid-box2').fadeIn(200);
+				$('.mask').fadeIn(200);
+			})
+$('.img-vi3').click(function() {
+				$('.vid-box3').fadeIn(200);
+				$('.mask').fadeIn(200);
+			})
 
 /*切换标签*/	
 $(".menu li").each(function(i){
@@ -163,4 +176,190 @@ $('.actGotop').click(function(){$('html,body').animate({scrollTop: 0}, 500);});
 
 
 })
+/*经销商*/
+$('.diaDealerHead li').each(function(i){
+	
+	$(this).click(
+		function () {
+			console.log(1)
+			$(".diaDealerHead li").removeClass('selected').eq(i).addClass('selected')
+		}
+	)
+})
+/*$(".menu li").each(function(i){
+	$(this).mouseover(function(){
+		$(".menu li").removeClass("cur").eq(i).addClass("cur");
+		$(".jx").hide().eq(i).show()		
+	})
+})
+	*/
+/*经销商弹窗*/
+ $(".dia-dealer").click(function() {
+        // console.log("dia-dealer onclick");
+        // $(".diaDealerHead ul li").removeClass('selected');
+        // $(".diaDealerHead ul li").first().addClass('selected');
+        // $.ajax({
+        //     dataType: "json",
+        //     url: "data/dealer_json.json",
+        //     complete: function(res) {
+        //         var data = jQuery.parseJSON(res.responseText);
+        //         // console.log(data["dong"]);
+        //         $.each(data["dong"], function(i, item) {
+        //             // console.log(i, item);
+
+        //             var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</tr>';
+        //             if (item["F"] == null) {
+        //                 var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '';
+        //             } else {
+        //                 var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '<div class="ac"></div> <div class="card"> <h3>车顾问</h3> <div class="avter"></div> <div class="nameBox"> <span class="name">' + item["F"] + '</span> <span class="post">客户经理</span> </div> <div class="codeBox"> <img src="' + item["G"] + '"/> <span>微信扫一扫  找我咨询</span> </div> </div> </td> </tr>';
+        //             }
+
+        //             $("#headTable").after(str);
+
+        //             // mouseObj();
+
+        //         })
+        //     }
+        // });
+
+        // $('.intro_box').fadeIn(200);
+        $('.mask').fadeIn(200);
+        $(".diaDealer").show();
+        $(".diaDealer").css('opacity','1');
+        // $(".diaDealerBg").show();
+    });
+
+    $("#d").click(function() {
+        $(".diaDealerHead ul li").removeClass('selected');
+        $(this).addClass('selected');
+        $("#headTable").nextAll().remove();
+        $.ajax({
+            dataType: "json",
+            url: "data/dealer_json.json",
+            complete: function(res) {
+                var data = jQuery.parseJSON(res.responseText);
+                // console.log(data["dong"]);
+                $.each(data["dong"], function(i, item) {
+                    // console.log(i, item);
+
+                    var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</tr>';
+                    if (item["F"] == null) {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '';
+                    } else {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '<div class="ac"></div> <div class="card"> <h3>车顾问</h3> <div class="avter"></div> <div class="nameBox"> <span class="name">' + item["F"] + '</span> <span class="post">客户经理</span> </div> <div class="codeBox"> <img src="' + item["G"] + '"/> <span>微信扫一扫  找我咨询</span> </div> </div> </td> </tr>';
+                    }
+
+                    $("#headTable").after(str);
+
+                    // mouseObj();
+
+                })
+            }
+        });
+        $(".diaDealer").show();
+    });
+
+    $("#b").click(function() {
+        $(".diaDealerHead ul li").removeClass('selected');
+        $(this).addClass('selected');
+        $("#headTable").nextAll().remove();
+        $.ajax({
+            dataType: "json",
+            url: "js/dealer_json.json",
+            complete: function(res) {
+                var data = jQuery.parseJSON(res.responseText);
+                // console.log(data["dong"]);
+                $.each(data["bei"], function(i, item) {
+                    // console.log(i, item);
+
+                    var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</tr>';
+                    if (item["F"] == null) {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '';
+                    } else {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '<div class="ac"></div> <div class="card"> <h3>车顾问</h3> <div class="avter"></div> <div class="nameBox"> <span class="name">' + item["F"] + '</span> <span class="post">客户经理</span> </div> <div class="codeBox"> <img src="' + item["G"] + '"/> <span>微信扫一扫  找我咨询</span> </div> </div> </td> </tr>';
+                    }
+
+                    $("#headTable").after(str);
+
+                    // mouseObj();
+
+                })
+            }
+        });
+        $(".diaDealer").show();
+    });
+
+    $("#x").click(function() {
+        $(".diaDealerHead ul li").removeClass('selected');
+        $(this).addClass('selected');
+        $("#headTable").nextAll().remove();
+        $.ajax({
+            dataType: "json",
+            url: "data/dealer_json.json",
+            complete: function(res) {
+                var data = jQuery.parseJSON(res.responseText);
+                // console.log(data["dong"]);
+                $.each(data["xi"], function(i, item) {
+                    // console.log(i, item);
+
+                    var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</tr>';
+                    if (item["F"] == null) {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '';
+                    } else {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '<div class="ac"></div> <div class="card"> <h3>车顾问</h3> <div class="avter"></div> <div class="nameBox"> <span class="name">' + item["F"] + '</span> <span class="post">客户经理</span> </div> <div class="codeBox"> <img src="' + item["G"] + '"/> <span>微信扫一扫  找我咨询</span> </div> </div> </td> </tr>';
+                    }
+
+                    $("#headTable").after(str);
+
+                    // mouseObj();
+
+                })
+            }
+        });
+        $(".diaDealer").show();
+    });
+
+    $("#n").click(function() {
+        $(".diaDealerHead ul li").removeClass('selected');
+        $(this).addClass('selected');
+        $("#headTable").nextAll().remove();
+        $.ajax({
+            dataType: "json",
+            url: "data/dealer_json.json",
+            complete: function(res) {
+                var data = jQuery.parseJSON(res.responseText);
+                // console.log(data["dong"]);
+                $.each(data["nan"], function(i, item) {
+                    // console.log(i, item);
+                    var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</tr>';
+                    if (item["F"] == null) {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '';
+                    } else {
+                        var str = '<tr><td>' + item["A"] + '</td> <td>' + item["B"] + '</td> <td>' + item["C"] + '</td> <td>' + item["D"] + '</td> <td>' + item["E"] + '<div class="ac"></div> <div class="card"> <h3>车顾问</h3> <div class="avter"></div> <div class="nameBox"> <span class="name">' + item["F"] + '</span> <span class="post">客户经理</span> </div> <div class="codeBox"> <img src="' + item["G"] + '"/> <span>微信扫一扫  找我咨询</span> </div> </div> </td> </tr>';
+                    }
+                    $("#headTable").after(str);
+                })
+            }
+        });
+        $(".diaDealer").show();
+    });
+
+
+
+    // function mouseObj() {
+    //     $(".ac").mouseenter(function() {
+    //         $(this).parent().find(".card").css({
+    //             "opacity": "1",
+    //             "height": "450px"
+    //         })
+    //         $(this).css("background-image", "url(img/ac-2.png)")
+    //     });
+    //     $(".ac").mouseleave(function() {
+    //         $(this).parent().find(".card").css({
+    //             "opacity": "0",
+    //             "height": "0"
+    //         })
+    //         $(this).css("background-image", "url(img/ac-1.png)")
+    //     });
+    // }
 
